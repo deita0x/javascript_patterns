489c9e2b23cd70598f089d2433dc8a195dc5e60c
deita0x <josdeita@gmail.com>
2014-08-07 14:02:08 -0500

    arreglos ortograficos

--- essentials/single_var_pattern.js
+++ essentials/single_var_pattern.js
@@ -1,8 +1,8 @@
-/* Single var pattern */ç
+/* Single var pattern */
 
 /* Usar una sola sentencia var al principio de las funciones brinda los siguientes beneficios:
- * Proporciona un unico lugar donde buscar todas las variables necesitadas por la funcion.
- * Previene errores logicos cuando una variables es usada sin ser declarada
+ * Proporciona un único lugar donde buscar todas las variables necesitadas por la función.
+ * Previene errores lógicos cuando una variables es usada sin ser declarada
  * Ayuda a recordar que las variables deben ser recordadas.
  */
 
@@ -17,7 +17,7 @@ function foo () {
   // function body ...
 }
 
-/* Tambien es buena practica inicializar las variables con un valor inicial, esto beneficia la
- * legibilidad del codigo y evita errores logicos (las variables no inicializadas tienen por defecto el valor undefined) 
- * Al mirar el codigo despues se puede de un  vistazo saber la intencion de uso de la variable: objeto, entero, cadena, etc.
+/* También es buena practica inicializar las variables con un valor inicial, esto beneficia la
+ * legibilidad del código y evita errores lógicos (las variables no inicializadas tienen por defecto el valor undefined) 
+ * Al mirar el codigo despues se puede de un  vistazo saber la intencion de uso de la variable: object, number, string, etc.
  */
